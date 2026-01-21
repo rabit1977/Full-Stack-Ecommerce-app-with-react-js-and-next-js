b460c1e0397839df518e3f82c1d81426a15c1d33
Rabit Ebibi <rabit2245@gmail.com>
2026-01-21 22:42:18 +0100

    feat: configure Next.js to externalize server packages and optimize remote images with specific patterns and security settings.

--- ecommerce-app/next.config.ts
+++ ecommerce-app/next.config.ts
@@ -37,6 +37,7 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    qualities: [75, 90],
   },
 };
 
